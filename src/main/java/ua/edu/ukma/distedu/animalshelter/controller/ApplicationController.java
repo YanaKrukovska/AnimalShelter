d0ea910cd51825442ded4eefb9e96fa1d5155618
YanaKrukovska <jana.krua@gmail.com>
2020-05-20 22:38:47 +0300

    - Requests are now shown on main page - Added methods to add requests from the main page

--- src/main/java/ua/edu/ukma/distedu/animalshelter/controller/ApplicationController.java
+++ src/main/java/ua/edu/ukma/distedu/animalshelter/controller/ApplicationController.java
@@ -1,25 +1,44 @@
 package ua.edu.ukma.distedu.animalshelter.controller;
 
-import ua.edu.ukma.distedu.animalshelter.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import ua.edu.ukma.distedu.animalshelter.persistence.model.Request;
+import ua.edu.ukma.distedu.animalshelter.service.AnimalService;
+import ua.edu.ukma.distedu.animalshelter.service.RequestService;
+import ua.edu.ukma.distedu.animalshelter.service.UserService;
+
+import java.util.Date;
 
 @Controller
 public class ApplicationController {
     private final UserService userService;
+    private final AnimalService animalService;
+    private final RequestService requestService;
 
     @Autowired
-    public ApplicationController(UserService userService) {
+    public ApplicationController(UserService userService, AnimalService animalService, RequestService requestService) {
         this.userService = userService;
+        this.animalService = animalService;
+        this.requestService = requestService;
     }
 
     @GetMapping("/")
     public String main(Model model) {
-    model.addAttribute("users", userService.getAllUsers());
-          return "index";
+        model.addAttribute("animals", animalService.getAllAnimals());
+        model.addAttribute("requests", requestService.getAllRequests());
+        return "index";
+    }
+
+    @PostMapping("/sendRequest")
+    public String sendRequest(Model model, @RequestParam Long animalId) {
+        model.addAttribute("animals", animalService.getAllAnimals());
+        model.addAttribute("requests", requestService.getAllRequests());
+        requestService.addRequest(new Request(userService.findUserById(2), animalService.findAnimalById(animalId), new Date()));
+        return "redirect:/";
     }
 
     @GetMapping("/users")
@@ -27,5 +46,6 @@ public class ApplicationController {
         model.addAttribute("users", userService.getAllUsers());
         return "users";
     }
+
 
 }
